9f3cfe5402836eab4cc5c5e41cde662cbf598d4a
Lizie Koehler & Jackie Fletcher <student@epicodus.com>
2015-01-20 13:37:15 -0800

    create a to-do list with sinatra and database

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -1,15 +1,33 @@
-require('rspec')
-require('to_do')
+require('spec_helper')
 
 describe(Task) do
-  before() do
-    Task.clear()
+  describe("#==") do
+    it("is the same task if it has the same description") do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "1/21/2015"})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1, :due_date =>"1/21/2015"})
+      expect(task1).to(eq(task2))
+    end
+  end
+
+
+  describe("#due_date") do
+    it("lets you give it a due-date") do
+      test_task = Task.new({:description => "scrub the zebra", :list_id => 1, :due_date => "1/21/2015"})
+      expect(test_task.due_date()).to(eq("1/21/2015"))
+    end
   end
 
   describe("#description") do
     it("lets you give it a description") do
-      test_task = Task.new("scrub the zebra")
+      test_task = Task.new({:description => "scrub the zebra", :list_id => 1, :due_date => "1/21/2015"})
       expect(test_task.description()).to(eq("scrub the zebra"))
+    end
+  end
+
+  describe("#list_id") do
+    it("lets you read the list ID out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "1/21/2015"})
+      expect(test_task.list_id()).to(eq(1))
     end
   end
 
@@ -21,17 +39,9 @@ describe(Task) do
 
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new("wash the lion")
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "1/21/2015"})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
-    end
-  end
-
-  describe(".clear") do
-    it("empties out all of the saved tasks") do
-      Task.new("wash the lion").save()
-      Task.clear()
-      expect(Task.all()).to(eq([]))
     end
   end
 end
